85a7a772e1e73a97ed7d9bb9983efbca2995d237
Hồ Đăng Quốc Đạt <hodangquocdat@gmail.com>
2018-12-12 11:41:29 +0700

    a

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.get('/apple-app-site-association', function (req, res) {
-  res.send('Hello World!')
+  res.send({ some: 'json' })
 })
 
 // catch 404 and forward to error handler
